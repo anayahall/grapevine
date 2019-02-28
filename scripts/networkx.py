1a79107b0d5561b75ba5e69c290edcdf8d020ec1
anayahall <anayahall@gmail.com>
2019-02-28 12:11:20 -0800

    moving along

--- scripts/networkx.py
+++
@@ -1,9 +0,0 @@
-# python network analysis course notes
-# feb. 2019
-# course: https://www.datacamp.com/community/tutorials/networkx-python-graph-tutorial#motivation
-
-# ensure all of these are installed
-pip install pandas
-pip install networkx
-pip install matplotlibpl
-
---
+++ scripts/parameters.py
@@ -0,0 +1,17 @@
+# parameters
+
+# BIOMASS
+waste_type
+
+
+# SWIS
+swf_type 	# solid waste facility type
+opc			# operating cost
+tc			# transportation cost
+
+
+# LAND
+cl_rate
+rl_rate
+
+
